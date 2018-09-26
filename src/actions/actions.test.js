9cec28e8e8f160abf9e504f3af054c776195a42b
nampdn <nampdn@protonmail.com>
2018-09-26 20:07:43 +0700

    test(actions): add sample unit on action

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,7 +1,18 @@
+import * as R from 'ramda';
 import * as ActionTypes from '.';
 
+const TYPES = [ActionTypes.SUCCESS, ActionTypes.FAILURE, ActionTypes.REQUEST];
+const ENTITIES = [
+  { name: 'TOPICS', data: ActionTypes.TOPICS },
+  { name: 'ITEMS', data: ActionTypes.ITEMS }
+];
 describe('action', () => {
-  it('should present a simple test', () => {
-    expect(ActionTypes).toHaveProperty('TOPICS');
+  it('should create three action type for each entity', () => {
+    R.forEach(entity => {
+      expect(ActionTypes).toHaveProperty(entity.name);
+      R.forEach(type => {
+        expect(ActionTypes[entity.name]).toHaveProperty(type);
+      }, TYPES);
+    }, ENTITIES);
   });
 });
