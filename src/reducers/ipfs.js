47f1ac2abb3670cfa3f8f884a2be0a496eaa53be
nampdn <nampdn@protonmail.com>
2018-09-29 09:06:15 +0700

    refactor(reducer): update ipfs reducer state

--- src/reducers/ipfs.js
+++ src/reducers/ipfs.js
@@ -1,18 +1,21 @@
 import { IPFS_CONFIG, SUCCESS } from '../actions';
 
-const initialState = {
+export const initialState = {
   api: 'QmPhxT9nZALsqWsn14AgkBichHQHXU73SAQx5FP8F6RfT9',
   gateways: ['https://ipfs.vgm.io', 'https://ipfs.io', 'https://storage3.bit.tube']
 };
 
-export default (state = initialState, action) => {
+const ipfsReducer = (state = initialState, action) => {
   switch (action.type) {
     case IPFS_CONFIG[SUCCESS]:
       return {
         ...state,
-        ...action.response
+        api: action.payload.api,
+        gateways: action.payload.gateways
       };
     default:
       return state;
   }
 };
+
+export default ipfsReducer;
