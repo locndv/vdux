33cb0f494c8f6d1ff7fb66fd8de9991d0c2567c4
nampdn <nampdn@protonmail.com>
2018-10-01 09:51:32 +0700

    feat(reducers): detach entities from main reducer
    this implement the topic entity first

---
+++ src/reducers/entities.js
@@ -0,0 +1,40 @@
+import * as R from 'ramda';
+import * as ActionTypes from '../actions';
+
+const initialState = {
+  topic: {},
+  item: {},
+  topicsCache: {},
+  itemsCache: {}
+};
+
+export const assignTopic = jsonData => () => ({
+  id: jsonData.id,
+  list: jsonData.list,
+  ...jsonData
+});
+
+export const addToCache = jsonData => cache => {
+  if (!R.prop(jsonData.url, cache)) {
+    return {
+      ...cache,
+      [jsonData.url]: jsonData
+    };
+  }
+  return cache;
+};
+
+const entitiesReducer = (state = initialState, action) => {
+  const payload = R.dissoc('reqId', action.payload);
+  switch (action.type) {
+    case ActionTypes.TOPICS.SUCCESS:
+      return R.evolve({
+        topic: assignTopic(payload),
+        topicsCache: addToCache(payload)
+      })(state);
+    default:
+      return state;
+  }
+};
+
+export default entitiesReducer;
