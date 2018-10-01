0265b437a2ec2d339dd836a19c19bb63bc0280fd
nampdn <nampdn@protonmail.com>
2018-10-01 09:53:41 +0700

    refactor(selectors): update selector of entities

--- src/reducers/selectors.js
+++ src/reducers/selectors.js
@@ -1,3 +1,3 @@
-export const getTopics = state => state.topics;
-export const getItems = state => state.items;
+export const getTopics = state => state.entities.topic;
+export const getItems = state => state.entities.item;
 export const getIPFS = state => state.ipfs;
