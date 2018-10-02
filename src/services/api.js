16bbfb77992b383c791dbf06a93ad08683542693
nampdn <nampdn@protonmail.com>
2018-10-02 11:09:12 +0700

    refactor(services): update fetchIPFSConfig endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -15,6 +15,6 @@ const callApi = endpoint => {
     .then(response => ({ response }), err => ({ error: err.message || 'Something bad happened' }));
 };
 
-export const fetchIPFSConfig = ipns => callApi(`/ipfs/${ipns}`);
+export const fetchIPFSConfig = ipns => callApi(`/ipns/${ipns}`);
 export const fetchTopics = (hash, topicUrl) => callApi(`/ipfs/${hash}/${topicUrl}.json`);
 export const fetchItems = (hash, itemUrl) => callApi(`/ipfs/${hash}/${itemUrl}.json`);
